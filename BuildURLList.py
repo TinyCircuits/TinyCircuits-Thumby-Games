e1bef465aab6633dbb3658569e8fbbd88d5f1296
jmtinycircuits <jmarcum@tinycircuits.com>
2021-11-03 18:25:53 -0400

    More testing

--- BuildURLList.py
+++ BuildURLList.py
@@ -1,6 +1,9 @@
 # https://canovasjm.netlify.app/2020/11/29/github-actions-run-a-python-script-on-schedule-and-commit-changes/
 
-print("Hello World!")
+import os
+print("Starting URL list builder using Python in GH Action...")
+
+print(os.listdir())
 
 f = open("URLList.txt", "w")
 f.write("TEST")
