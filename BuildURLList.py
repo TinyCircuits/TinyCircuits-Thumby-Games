0ef61f1c734c309ab3e2905a6c3365f735ef2b84
jmtinycircuits <jmarcum@tinycircuits.com>
2021-11-03 18:21:11 -0400

    Add more testing

--- BuildURLList.py
+++ BuildURLList.py
@@ -1,1 +1,7 @@
+# https://canovasjm.netlify.app/2020/11/29/github-actions-run-a-python-script-on-schedule-and-commit-changes/
+
 print("Hello World!")
+
+f = open("URLList.txt", "w")
+f.write("TEST")
+f.close()
