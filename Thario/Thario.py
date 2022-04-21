5761cc62afbc53371269d247f7fde988ceef7b8b
Jeroen Peters <jeroenpeters1986@gmail.com>
2022-04-22 00:40:29 +0200

    Version 1.1 of Thario, it contains more graphics and now you can collect coins

--- Thario/Thario.py
+++ Thario/Thario.py
@@ -23,12 +23,10 @@
 import ssd1306
 import machine
 import time
-import uos
 import random
 import gc
 import utime
 import thumby
-import os
 
 machine.freq(125000000)
 gc.enable() # This line helps make sure we don't run out of memory
@@ -45,6 +43,7 @@ Points = 0
 GameRunning = True
 EnemyPos = random.randint(72, 300)
 CloudPos = random.randint(60, 200)
+CoinPos = random.randint(60, 200)
 JumpSoundTimer = 0
 
 def fscore(s):
@@ -60,15 +59,28 @@ PlayerRunFrame1 = bytearray([255,255,255,199,89,96,104,124,124,80,205,221,221,25
 PlayerRunFrame2 = bytearray([255,255,255,71,25,96,104,124,124,80,205,221,221,255,255,255,255,159,15,6,64,192,224,226,192,192,192,132,129,195,255,255])
 PlayerRunFrame3 = bytearray([255,255,255,143,179,193,209,249,249,161,155,187,187,255,255,255,159,135,199,199,192,0,0,0,0,64,231,255,255,255,255,255])
 
-CloudSpr = bytearray([0x9F, 0x4F, 0x63, 0x59, 0xBD, 0x73, 0x73, 0x65, 0x5C, 0x7E, 0x7E, 0x51, 0x57, 0x4F, 0x1F, 0xBF])
+# Sky decorations: BigCloud, Sun, LittleCloud
+# BITMAP: width: 16, height: 8
+SkySets = [bytearray([159,79,99,89,189,115,115,101,92,126,126,81,87,79,31,191]),
+           bytearray([255,247,190,213,107,156,221,92,221,156,107,213,190,247,255,255]),
+           bytearray([255,255,255,255,255,145,110,126,106,222,106,126,110,145,255,255])]
+CloudSpr = SkySets[0]
 
+# Enemies: Flower, Ghost, Mushroom, Hedgehog, Pipe, Toad
 # BITMAP: width: 8, height: 12
-#Enemies: Flower, Ghost, Toad
 EnemySets = [[bytearray([115,65,202,31,31,202,224,243,14,14,12,0,0,9,12,14]), bytearray([63,99,193,10,26,192,97,127,15,14,12,0,0,9,12,15])],
              [bytearray([1,115,87,119,87,119,1,51,8,0,1,4,13,8,0,3]), bytearray([1,99,87,119,85,97,1,55,0,1,1,12,13,1,0,11])],
-             [bytearray([143,131,25,0,0,25,131,143,8,0,1,7,15,7,0,9]), bytearray([143,131,49,0,0,49,131,143,9,0,7,15,3,1,0,8])]]
+             [bytearray([143,131,25,0,0,25,131,143,8,0,1,7,15,7,0,9]), bytearray([143,131,49,0,0,49,131,143,9,0,7,15,3,1,0,8])],
+             [bytearray([63,135,27,135,91,135,91,7,14,2,0,2,9,2,1,0]), bytearray([31,67,45,67,173,67,173,3,14,2,2,0,14,2,2,0])],
+             [bytearray([248,2,250,250,10,250,2,248,15,0,15,15,8,15,0,15]), bytearray([248,2,250,250,10,250,2,248,15,0,15,15,8,15,0,15])],
+             [bytearray([243,230,152,18,101,235,239,31,15,15,15,7,2,0,10,15]), bytearray([231,197,153,21,73,215,223,63,15,15,14,2,0,0,5,14])]]
 EnemySet = EnemySets[0]
 
+# Coin
+# BITMAP: width: 6, height: 8
+CoinSpr = bytearray([129,126,94,66,126,129])
+
+# TitleScreen
 thumby.display.fill(0)
 thumby.display.setFont("/lib/font8x8.bin", 8, 8, 0)
 thumby.display.drawText("Thar", 0, 10, 1)
@@ -121,7 +133,7 @@ while GameRunning:
     # Handle "dynamics"
     YPos += YVel
     YVel += Gravity
-    Points += XVel
+    Points += (XVel/2)
     JumpSoundTimer -= 15
 
     if JumpSoundTimer > 0:
@@ -138,7 +150,7 @@ while GameRunning:
         YVel = 0.0
 
     # Did Thario hit an enemy?
-    if EnemyPos < 8 and EnemyPos > -8 and YPos > -8:
+    if EnemyPos < 7 and EnemyPos > -8 and YPos > -8:
         # Stop the game and give a prompt
         GameRunning = False
 
@@ -174,32 +186,48 @@ while GameRunning:
                 GameRunning = True
                 EnemyPos = random.randint(72, 300)
                 CloudPos = random.randint(60, 200)
+                CoinPos = random.randint(60, 200)
 
             elif thumby.buttonB.pressed() == True:
                 # Quit
                 machine.reset()
 
+    # Did Thario collect a coin?
+    if (CoinPos > 0 and CoinPos < 8) and YPos < -4:
+        thumby.audio.play(3136, 300)
+        Points += 25
+        CoinPos = random.randint(int(104+CloudPos), 300)
+
     # Is the enemy out of view?
     if EnemyPos < -24:
         Points += 10
         thumby.audio.play(440, 300)
-        EnemyPos = random.randint(72, 500)
+        EnemyPos = random.randint(72, 450)
 
         randomEnemy = random.randint(0, len(EnemySets)-1)
         EnemySet = EnemySets[randomEnemy]
 
     # Is the cloud out of view?
-    if(CloudPos < -32):
+    if CloudPos < -32:
         # "spawn" another one
-        CloudPos = random.randint(40, 200)
+        CloudPos = random.randint(72, 180)
+        randomSky = random.randint(0, len(SkySets)-1)
+        CloudSpr = SkySets[randomSky]
+
+    # Is the coin out of view?
+    if CoinPos < -12:
+        CoinPos = random.randint(72, 180)
 
     # More dynamics
     EnemyPos -= XVel * 16
     CloudPos -= XVel * 2
+    CoinPos -= XVel * 4
 
     # Draw game state
     thumby.display.fill(1)
     thumby.display.blit(CloudSpr, int(16 + CloudPos), 8, 16, 8, 1, 0, 0)
+    thumby.display.drawFilledRectangle(int(6 + CoinPos), 8, 6, 8, 1) # Coin background so it 'overwrites' clouds
+    thumby.display.blit(CoinSpr, int(6 + CoinPos), 8, 6, 8, 1, 0, 0)
 
     if t0 % 250000 < 125000 or YPos != 0.0:
         # Player is in first frame of run animation
@@ -213,6 +241,7 @@ while GameRunning:
     else:
         thumby.display.blit(EnemySet[1], int(16 + EnemyPos), 28, 8, 12, 1, 0, 0)
 
+    thumby.display.drawText(fscore(int(Points)), 2, 1, 1) # Current points backdrop
     thumby.display.drawText(fscore(int(Points)), 1, 1, 0) # Current points
     thumby.display.update()
 
